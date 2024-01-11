096741a4edc4c4dc4a492e8059ed760d1ad87ed5
abu <abu@test.com>
2024-01-11 11:18:35 +0000

    added vpc dir

---
+++ vpc/main.tf
@@ -0,0 +1,39 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "ab1-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ab1-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ab1-public-subnet-2"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
