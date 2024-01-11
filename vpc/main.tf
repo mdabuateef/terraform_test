6cf067562c0544cc3d83d9b3884d88cd49ad1b59
mdabuateef <mdabuateef@gmail.com>
2024-01-11 17:45:59 +0530

    .

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,6 +7,28 @@ resource "aws_vpc" "main" {
   }
 }
 
+//create an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+//create an public rt
+resource "aws_route_table" "public-rt"{
+  vpc_id = aws_vpc.main.id
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  route {
+    cidr_block = aws_vpc.main.cidr_block
+    gateway_id = "local"
+  }
+}
+
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -27,6 +49,18 @@ resource "aws_subnet" "public_subnet_2" {
   tags = {
     Name = "ab1-public-subnet-2"
   }
+}
+
+//route table association
+
+resource "aws_route_table_association" "rt-1-association"{
+  subnet_id = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "rt-2-association"{
+  subnet_id = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public-rt.id
 }
 
 
