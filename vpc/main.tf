8484c51f81a607fa905737d824a1ea81ec5f01d9
mdabuateef <mdabuateef@gmail.com>
2024-01-11 17:40:41 +0530

    .

--- vpc/main.tf
+++ vpc/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 
   tags = {
